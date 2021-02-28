151d7102b57b7ba3859aa8fb0f4d76bb046732d3
rreinold <rreinold@gmail.com>
2021-02-28 12:51:02 -0600

    added forbidden paths

--- util/util.go
+++ util/util.go
@@ -5,7 +5,7 @@ import (
 )
 
 func IsForbiddenPath(input string) bool {
-	REGEXP := `\.\.`
+	REGEXP := `(\.\.|~)`
 	match, err := regexp.Match(REGEXP, []byte(input))
 	if err != nil {
 		return true
